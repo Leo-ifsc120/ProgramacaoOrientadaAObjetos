df1036449f98682d4628382300ae8c2c8aea4eed
Aluno <Leonardo.d2006@aluno.ifsc.edu.br>
2024-11-28 14:46:00 -0300

    Update aula

---
+++ PrjLavaCarrosTestes/src/Main/Main.java
@@ -0,0 +1,85 @@
+package Main;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class Main extends JDialog
+{
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField placa;
+    private JTextField cor;
+    private JTextField modelo;
+    private JTextField marca;
+    private JTextField potencia;
+    private JComboBox categoria;
+    private JComboBox combustivel;
+
+    public Main()
+    {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+
+
+        buttonOK.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK()
+    {
+
+    }
+
+    private void onCancel()
+    {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args)
+    {
+        Main dialog = new Main();
+        dialog.setSize(480,360);
+        dialog.setLocationRelativeTo(null);
+        dialog.setTitle("Insira os dados do carro");
+        dialog.setVisible(true);
+        System.exit(0);
+
+    }
+}
