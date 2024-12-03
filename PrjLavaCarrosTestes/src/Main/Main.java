b176cb8f09050e998e97e33c585fd69a9247d8f5
Aluno <leonardo.d2006@aluno.ifsc.edu.br>
2024-12-03 15:43:10 -0300

    Update Aula

--- PrjLavaCarrosTestes/src/Main/Main.java
+++ PrjLavaCarrosTestes/src/Main/Main.java
@@ -18,6 +18,7 @@ public class Main extends JDialog
     private JTextField potencia;
     private JComboBox categoria;
     private JComboBox combustivel;
+    private JTextField textField1;
 
     public Main()
     {
@@ -81,6 +82,7 @@ public class Main extends JDialog
         Marca marca1 = new Marca();
         Modelo modelo1 = new Modelo();
         Veiculo veiculo1 = new Veiculo();
+        Cor cor1 = new Cor();
 
 
         Main dialog = new Main();
@@ -88,15 +90,70 @@ public class Main extends JDialog
         dialog.setLocationRelativeTo(null);
         dialog.setTitle("Insira os dados do carro");
         dialog.setVisible(true);
+
+        veiculo1.setPlaca(dialog.placa.getText());
+        marca1.setNome(dialog.marca.getText());
+        modelo1.setDescricao(dialog.modelo.getText());
+        cor1.setNome(dialog.cor.getText());
+
+        veiculo1.setModelo(modelo1);
+        veiculo1.getModelo().setMarca(marca1);
+        veiculo1.setCor(cor1);
+        veiculo1.getModelo().getMotor().setPotencia(Integer.parseInt(dialog.potencia.getText()));
+
+        switch(dialog.categoria.getSelectedIndex())
+        {
+            case 0:
+                veiculo1.getModelo().setCategoria(ECategoria.eCategoria.Pequeno);
+                break;
+            case 1:
+                veiculo1.getModelo().setCategoria(ECategoria.eCategoria.Padrao);
+                break;
+            case 2:
+                veiculo1.getModelo().setCategoria(ECategoria.eCategoria.Medio);
+                break;
+            case 3:
+                veiculo1.getModelo().setCategoria(ECategoria.eCategoria.Grande);
+                break;
+            case 4:
+                veiculo1.getModelo().setCategoria(ECategoria.eCategoria.Moto);
+                break;
+        }
+
+        switch(dialog.combustivel.getSelectedIndex())
+        {
+            case 0:
+                veiculo1.getModelo().getMotor().setTipoCombustivel(ETipoCombustivel.eTipoCombustivel.Gasolina);
+                break;
+            case 1:
+                veiculo1.getModelo().getMotor().setTipoCombustivel(ETipoCombustivel.eTipoCombustivel.Etanol);
+                break;
+            case 2:
+                veiculo1.getModelo().getMotor().setTipoCombustivel(ETipoCombustivel.eTipoCombustivel.Flex);
+                break;
+            case 3:
+                veiculo1.getModelo().getMotor().setTipoCombustivel(ETipoCombustivel.eTipoCombustivel.Diesel);
+                break;
+            case 4:
+                veiculo1.getModelo().getMotor().setTipoCombustivel(ETipoCombustivel.eTipoCombustivel.GNV);
+                break;
+            case 5:
+                veiculo1.getModelo().getMotor().setTipoCombustivel(ETipoCombustivel.eTipoCombustivel.Outro);
+                break;
+        }
+
+
         Show print = new Show();
         print.setSize(720,360);
         print.setLocationRelativeTo(null);
         print.setTitle("Dados informados");
-        print.setPlaca(dialog.placa.getText());
-        print.setCor(dialog.cor.getText());
-        print.setModelo(dialog.modelo.getText());
-        print.setMarca(dialog.marca.getText());
-        print.setPotencia(dialog.potencia.getText());
+        print.setPlaca(veiculo1.getPlaca());
+        print.setCor(veiculo1.getCor().getNome());
+        print.setModelo(veiculo1.getModelo().getDescricao());
+        print.setMarca(veiculo1.getModelo().getMarca().getNome());
+        print.setPotencia(String.valueOf(veiculo1.getModelo().getMotor().getPotencia()));
+        print.setCategoria(veiculo1.getModelo().getCategoria().toString());
+        print.setCombustivel(veiculo1.getModelo().getMotor().getTipoCombustivel().toString());
         print.setVisible(true);
         System.exit(0);
 
--- PrjLavaCarrosTestes/src/Main/Show.java
+++ PrjLavaCarrosTestes/src/Main/Show.java
@@ -1,6 +1,7 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Show extends JDialog
 {
@@ -12,13 +13,65 @@ public class Show extends JDialog
     private JLabel potencia;
     private JLabel categoria;
     private JLabel combustivel;
+    private JButton gravarNovamenteButton;
+    private JButton cancelarButton;
     private JButton buttonOK;
 
     public Show()
     {
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
+        getRootPane().setDefaultButton(gravarNovamenteButton);
+
+
+        gravarNovamenteButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                onOK();
+            }
+        });
+
+        cancelarButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+
+    }
+
+    private void onOK()
+    {
+        dispose();
+    }
+
+    private void onCancel()
+    {
+        // add your code here if necessary
+        dispose();
+        System.exit(0);
     }
 
     public void setPlaca(String placa)
