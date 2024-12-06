f3b0d46ae4a9c6fb469b0e41503d54253d688655
Aluno <leonardo.d2006@aluno.ifsc.edu.br>
2024-12-06 13:21:31 -0300

    Update aula

--- PrjLavaCarrosA3/src/domain/Pontuacao.java
+++ PrjLavaCarrosA3/src/domain/Pontuacao.java
@@ -6,9 +6,23 @@ public class Pontuacao
     private int quantidade;
 
 
-    public int getQuantidade()
+    public int saldo()
     {
         return quantidade;
+    }
+
+    public void adicionar(int valor)
+    {
+
+        this.quantidade = this.quantidade + valor;
+
+    }
+
+    public void subtrair(int valor)
+    {
+
+        this.quantidade = this.quantidade - valor;
+
     }
 
 }
